987989e108c188c8c0f03701653bfb7a0abfc9cb
Diter Evan <hansevangelista@gmail.com>
2024-07-05 15:09:12 -0500

    'add changes

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -38,7 +38,7 @@
   <div class="navbar bg-base-100">
     <div class="navbar-start">
       <details class="dropdown">
-        <div tabindex="0" role="button" class="btn btn-ghost lg:hidden">
+        <summary tabindex="0" role="button" class="btn btn-ghost lg:hidden">
           <svg
             xmlns="http://www.w3.org/2000/svg"
             class="h-5 w-5"
@@ -51,7 +51,7 @@
               stroke-width="2"
               d="M4 6h16M4 12h8m-8 6h16" />
           </svg>
-        </div>
+        </summary>
         <ul
           tabindex="0"
           class="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
