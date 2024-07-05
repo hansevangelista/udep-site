30eae9711fe3f54698fda33b58f1badff1bffdd4
Diter Evan <hansevangelista@gmail.com>
2024-07-05 15:27:47 -0500

    'add changes

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,37 +3,6 @@
 </script>
 
 <template>
-  <details class="dropdown">
-    <summary class="btn m-1">open or close</summary>
-    <ul class="menu dropdown-content bg-base-100 rounded-box z-[1] w-52 p-2 shadow">
-      <li><a>Item 1</a></li>
-      <li><a>Item 2</a></li>
-    </ul>
-  </details>
-
-  <ul class="menu lg:menu-horizontal bg-base-200 rounded-box lg:mb-64">
-    <li><a>Item 1</a></li>
-    <li>
-      <details open>
-        <summary>Parent item</summary>
-        <ul>
-          <li><a>Submenu 1</a></li>
-          <li><a>Submenu 2</a></li>
-          <li>
-            <details open>
-              <summary>Parent</summary>
-              <ul>
-                <li><a>item 1</a></li>
-                <li><a>item 2</a></li>
-              </ul>
-            </details>
-          </li>
-        </ul>
-      </details>
-    </li>
-    <li><a>Item 3</a></li>
-  </ul>
-
 
   <div class="navbar bg-base-100">
     <div class="navbar-start">
@@ -66,59 +35,6 @@
           <li><a>Item 3</a></li>
         </ul>
       </details>
-      <a class="btn btn-ghost text-xl">daisyUI</a>
-    </div>
-    <div class="navbar-center hidden lg:flex">
-      <ul class="menu menu-horizontal px-1">
-        <li><a>Item 1</a></li>
-        <li>
-          <details>
-            <summary>Parent</summary>
-            <ul class="p-2">
-              <li><a>Submenu 1</a></li>
-              <li><a>Submenu 2</a></li>
-            </ul>
-          </details>
-        </li>
-        <li><a>Item 3</a></li>
-      </ul>
-    </div>
-    <div class="navbar-end">
-      <a class="btn">Button</a>
-    </div>
-  </div>
-
-  <div class="navbar bg-base-100">
-    <div class="navbar-start">
-      <div class="dropdown">
-        <div tabindex="0" role="button" class="btn btn-ghost lg:hidden">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            class="h-5 w-5"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor">
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
-              d="M4 6h16M4 12h8m-8 6h16" />
-          </svg>
-        </div>
-        <ul
-          tabindex="0"
-          class="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
-          <li><a>Item 1</a></li>
-          <li>
-            <a>Parent</a>
-            <ul class="p-2">
-              <li><a>Submenu 1</a></li>
-              <li><a>Submenu 2</a></li>
-            </ul>
-          </li>
-          <li><a>Item 3</a></li>
-        </ul>
-      </div>
       <a class="btn btn-ghost text-xl">daisyUI</a>
     </div>
     <div class="navbar-center hidden lg:flex">
