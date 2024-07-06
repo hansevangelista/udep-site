ff926777e4846cc4d51f33e0b04ff321a25593ba
Diter Evan <hansevangelista@gmail.com>
2024-07-05 19:34:51 -0500

    add changes

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -24,33 +24,19 @@
         <ul
           tabindex="0"
           class="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
-          <li><a>Item 1</a></li>
-          <li>
-            <a>Parent</a>
-            <ul class="p-2">
-              <li><a>Submenu 1</a></li>
-              <li><a>Submenu 2</a></li>
-            </ul>
-          </li>
-          <li><a>Item 3</a></li>
+          <li><a>Home</a></li>
+          <li><a>Premios</a></li>
+          <li><a>Ranking</a></li>
         </ul>
       </details>
 
       <img src="@/assets/logo.png" class="h-12 lg:h-16">
     </div>
     <div class="navbar-center hidden lg:flex">
-      <ul class="menu menu-horizontal px-1">
-        <li><a>Item 1</a></li>
-        <li>
-          <details>
-            <summary>Parent</summary>
-            <ul class="p-2">
-              <li><a>Submenu 1</a></li>
-              <li><a>Submenu 2</a></li>
-            </ul>
-          </details>
-        </li>
-        <li><a>Item 3</a></li>
+      <ul class="menu menu-horizontal px-1" style="color: #fff; font-size: 16px; font-weight: bold;">
+        <li><a>Home</a></li>
+        <li><a>Premios</a></li>
+        <li><a>Ranking</a></li>
       </ul>
     </div>
     <div class="navbar-end">
